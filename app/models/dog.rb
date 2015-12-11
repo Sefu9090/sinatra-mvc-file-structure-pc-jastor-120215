39c9f4165dac165d091c172e74fe41c4ed1a27ef
Saul Fernandez <fsaul16@yahoo.com>
2015-12-11 01:20:58 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,24 @@
+class Dog 
+  attr_accessor :age
+  attr_accessor :name
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+
+  def breed
+    @breed
+  end
+  
+
+  
+  
+  def name
+    @name
+  end
+  
+  def age
+    @age
+  end
+end
